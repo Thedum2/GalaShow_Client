a7dafa8b8fa4b22c6480f286388c83efd984037d
Thedum2 <asdf74591@gmail.com>
2025-09-28 14:11:06 +0900

    `feat: 새로운 페이지 컴포넌트 및 라우트 추가`

--- src/routes/config.tsx
+++ src/routes/config.tsx
@@ -7,6 +7,11 @@ import {PATHS} from "@/routes/paths";
 
 const SimulationSamplePage = lazy(() => import("@/components/sample/SimulationSamplePage"));
 const WelcomePage = lazy(() => import("@/pages/Welcome"));
+const LobbyPage = lazy(() => import("@/pages/Lobby"));
+const SelectPage = lazy(() => import("@/pages/Select"));
+const LoadingPage = lazy(() => import("@/pages/Loading"));
+const ResultPage = lazy(() => import("@/pages/Result"));
+const WinnerPage = lazy(() => import("@/pages/Winner"));
 
 
 export const routes: RouteObject[] = [
@@ -15,6 +20,11 @@ export const routes: RouteObject[] = [
         errorElement: <ErrorBoundary/>,
         children: [
             {index: true, path: PATHS.welcome, element: <Suspense fallback={<div>Loading…</div>}><WelcomePage/></Suspense>},
+            {path: PATHS.lobby, element: <Suspense fallback={<div>Loading…</div>}><LobbyPage/></Suspense>},
+            {path: PATHS.select, element: <Suspense fallback={<div>Loading…</div>}><SelectPage/></Suspense>},
+            {path: PATHS.loading, element: <Suspense fallback={<div>Loading…</div>}><LoadingPage/></Suspense>},
+            {path: PATHS.result, element: <Suspense fallback={<div>Loading…</div>}><ResultPage/></Suspense>},
+            {path: PATHS.winner, element: <Suspense fallback={<div>Loading…</div>}><WinnerPage/></Suspense>},
             {path: PATHS.simulation_sample,    element: <Suspense fallback={<div>Loading…</div>}><SimulationSamplePage/></Suspense>},
             {path: "*", element: <NotFoundPage/>},
         ]
--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -1,5 +1,10 @@
 export const PATHS = {
     root: "/",
     welcome: "/",
+    lobby: "/lobby",
+    select: "/select",
+    loading: "/loading",
+    result: "/result",
+    winner: "/winner",
     simulation_sample: "/simulation_sample",
 } as const;
