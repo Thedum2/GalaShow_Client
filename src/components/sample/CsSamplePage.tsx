f71776fe6f69d63226cf5a627898223ed3d1c99a
Thedum2 <asdf74591@gmail.com>
2025-10-05 00:18:15 +0900

    `feat: polychat-bridge 라이브러리 도입 및 샘플 버튼 추가`

--- src/components/sample/CsSamplePage.tsx
+++ src/components/sample/CsSamplePage.tsx
@@ -1,7 +1,23 @@
-﻿export default function CsSamplePage() {
+﻿import Icon from "@/components/icons";
+import StartGameButton from "@/components/lobby/StartGameButton";
+import React from "react";
+import {hello} from "polychat-bridge";
+
+export default function CsSamplePage() {
     return (
         <div>
             CsSamplePage
+
+            <StartGameButton
+                text="Hello"
+                icon={<Icon name="bookmark" size={35} mode="eager"/>}
+                backgroundColor="#FFDE59"
+                textColor="#000000"
+                onClick={() => {
+                    alert(hello('Cs_Sample'));
+                }}
+            />
         </div>
+
     );
 }
