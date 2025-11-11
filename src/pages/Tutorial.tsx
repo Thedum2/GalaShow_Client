0ff7e2d859ee0768cdf8b6f1ffb9ebb4992e382c
Thedum2 <asdf74591@gmail.com>
2025-11-11 13:16:57 +0900

    `feat: UnityPlayer 컴포넌트 Tutorial 페이지에 통합`

--- src/pages/Tutorial.tsx
+++ src/pages/Tutorial.tsx
@@ -1,11 +1,34 @@
-﻿export default function Tutorial() {
+﻿import UnityPlayer from "@/bridge/UnityPlayer";
+import {useUnity} from "@/bridge/useUnity";
+
+export default function Tutorial() {
+    const {unityProvider, isLoaded, loadingProgression} = useUnity();
 
     return (
 
         <div className="flex h-full w-full pr-10 pl-10 pt-3 pb-3 gap-6">
             {/* 1. 좌측 영역 */}
-            <div className="flex flex-1 flex-col gap-3 border-2 border-green-500">
+            <div className="flex flex-1 flex-col gap-3">
 
+                {/* 2-1. 상단 */}
+                <div className="h-[130px] w-full border-2 flex border-green-600"></div>
+
+
+                {/* 2-1. 중단 */}
+                <div className="flex-1 w-full border-2 flex border-yellow-600">
+
+                    <UnityPlayer
+                        unityProvider={unityProvider}
+                        isLoaded={isLoaded}
+                        loadingProgression={loadingProgression}
+                    />
+
+
+                </div>
+
+
+                {/* 2-1. 하단 */}
+                <div className="h-[130px] w-full border-2 flex border-white"></div>
             </div>
 
             {/* 2. 우측 영역 */}
